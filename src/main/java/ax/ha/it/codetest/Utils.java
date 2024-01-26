b4d9ed6bbe4faddc79203dce83161e896cdf3479
anna <anna.soderlund@ha.ax>
2024-01-26 10:26:02 +0200

    Added function power

--- src/main/java/ax/ha/it/codetest/Utils.java
+++ src/main/java/ax/ha/it/codetest/Utils.java
@@ -1,8 +1,11 @@
 package ax.ha.it.codetest;
 
 public class Utils {
-    static double power(double base, double exponent){
-        //implementation here
-        return 0;
+    static double power(double base, int exponent){
+        double result=1;
+        for(int i = exponent; i!=0; i--){
+            result=result*base;
+        }
+        return result;
     }
 }
