1509a431f3c5637f5a6296d900fdc627f18f793d
anna <anna.soderlund@ha.ax>
2024-01-26 11:43:05 +0200

    renamed calcPayment to calcPaymentTest

--- src/test/java/ax/ha/it/codetest/CustomerTest.java
+++ src/test/java/ax/ha/it/codetest/CustomerTest.java
@@ -19,12 +19,12 @@ class CustomerTest {
 
     @ParameterizedTest
     @CsvSource({
-            "name, 1000, 2, 5, 28.76796583", //loan integer, interest integer
-            "name, 2000.50, 2, 5, 57.55031563", //loan double, interest integer
-            "name, 3000, 2.5, 5, 97.0601877", //loan integer, interest double
-            "name, 4000.50, 2.5, 5, 129.4297603" //loan double, interest double
+            "name, 1000, 2, 5, 17.52776005", //loan integer, interest integer
+            "name, 2000.50, 2, 5, 35.06428399", //loan double, interest integer
+            "name, 3000, 2.5, 5, 53.24208482", //loan integer, interest double
+            "name, 4000.50, 2.5, 5, 70.9983201" //loan double, interest double
     })
-    void calcPayment(String name, double loan, double interest, int years, double expResult) {
+    void calcPaymentTest(String name, double loan, double interest, int years, double expResult) {
         Customer customer = new Customer(name,loan,interest,years);
         assertEquals(expResult, customer.calcPayment(),1e-7);
     }
